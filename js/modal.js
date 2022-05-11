79c9faca86b68a0883f8d47704ad532226c21e2f
worashf <uppert83@gmail.com>
2022-05-11 22:38:00 +0300

    close modal function

--- js/modal.js
+++ js/modal.js
@@ -1,145 +1,142 @@
- const projects =[
-     { projectId:"project-1",
-       projectName:"Multi Post Stories",
-       projectImage:"/images/post-img.svg",
-       languages:[
-        'CSS',
-        'HTML',
-        'Bootsrap',
-        'Ruby',
-        
-       ],
-       liveLink:"",
-       sourceLink:"",
-       description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text."
-     },{
-        projectId:"project-2",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-    },
-    {
-        projectId:"project-3",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-      },
-      {
-        projectId:"project-4",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-      },
-      {
-        projectId:"project-5",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-      },
-      {
-        projectId:"project-6",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-      },
-      {
-        projectId:"project-7",
-        projectName:"Profesional Art Printing Data",
-        projectImage:"/images/story-img.svg",
-        languages:[
-         
-         'HTML',
-         'Bootsrap',
-         'Ruby',
-         
-        ],
-        liveLink:"",
-        sourceLink:"",
-        description:" A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard "
-      }     
- ]
+const projects = [
+  {
+    projectId: 'project-1',
+    projectName: 'Multi Post Stories',
+    projectImage: './images/post-img.svg',
+    languages: [
+      'CSS',
+      'HTML',
+      'Bootsrap',
+      'Ruby',
 
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  }, {
+    projectId: 'project-2',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
 
+      'HTML',
+      'Bootsrap',
+      'Ruby',
 
- let projectModal = document.getElementById("modal-container");
- let buttons=document.querySelectorAll("button");
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+  {
+    projectId: 'project-3',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
 
+      'HTML',
+      'Bootsrap',
+      'Ruby',
 
-  let projectButtons =[];
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+  {
+    projectId: 'project-4',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
 
-  buttons.forEach((btn)=>{
-       const [button] = btn.id.split('-');
-       if(button==="project"){
-           projectButtons.push(btn);
-       }
-  })
+      'HTML',
+      'Bootsrap',
+      'Ruby',
 
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+  {
+    projectId: 'project-5',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
 
-  function openProjectModal(projectId){
+      'HTML',
+      'Bootsrap',
+      'Ruby',
 
-  let project = projects.find((pro) =>pro.projectId === projectId);
-  let language =""
-  project.languages.forEach((lang) =>{
-      language+= `<li> ${lang} </li>`
-  });
-   document.getElementById("project-name").innerHTML= project.projectName;
-   document.getElementById("languages-list").innerHTML=language;
-   document.getElementById("project-image").src=project.projectImage;
-   document.getElementById("modal-description").textContent=project.description;
-   document.getElementById("live-link").href=project.liveLink;
-   document.getElementById("source-link").href =project.sourceLink;
-   projectModal.style.display="block";
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+  {
+    projectId: 'project-6',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
 
- 
+      'HTML',
+      'Bootsrap',
+      'Ruby',
+
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+  {
+    projectId: 'project-7',
+    projectName: 'Profesional Art Printing Data',
+    projectImage: './images/story-img.svg',
+    languages: [
+
+      'HTML',
+      'Bootsrap',
+      'Ruby',
+
+    ],
+    liveLink: '',
+    sourceLink: '',
+    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+  },
+];
+
+const projectModal = document.getElementById('modal-container');
+const buttons = document.querySelectorAll('button');
+const closeBtn = document.querySelector('.modal-close');
+
+const projectButtons = [];
+
+buttons.forEach((btn) => {
+  const [button] = btn.id.split('-');
+  if (button === 'project') {
+    projectButtons.push(btn);
   }
+});
 
-
-  projectButtons.forEach((projectButton) => {
-    const { id} = projectButton;
-    projectButton.addEventListener('click', () => openProjectModal(id));
+function openProjectModal(projectId) {
+  const project = projects.find((pro) => pro.projectId === projectId);
+  let language = '';
+  project.languages.forEach((lang) => {
+    language += `<li> ${lang} </li>`;
   });
+  document.getElementById('project-name').innerHTML = project.projectName;
+  document.getElementById('languages-list').innerHTML = language;
+  document.getElementById('project-image').src = project.projectImage;
+  document.getElementById('modal-description').textContent = project.description;
+  document.getElementById('live-link').href = project.liveLink;
+  document.getElementById('source-link').href = project.sourceLink;
+  projectModal.style.display = 'block';
+}
+
+projectButtons.forEach((projectButton) => {
+  const { id } = projectButton;
+  projectButton.addEventListener('click', () => openProjectModal(id));
+});
+closeBtn.addEventListener('click', () => {
+  projectModal.style.display = 'none';
+});
