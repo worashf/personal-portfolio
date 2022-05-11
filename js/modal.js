f619acde0c9b933e92e6ccc1fe3f18e1bf54bf3a
worashf <uppert83@gmail.com>
2022-05-11 23:25:43 +0300

    remove linters error

--- js/modal.js
+++ js/modal.js
@@ -101,7 +101,7 @@ const projects = [
     ],
     liveLink: '',
     sourceLink: '',
-    description: " A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard ",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
   },
 ];
 
