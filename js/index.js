74c37cad2ed12192fd63d47f18d9e7e63836c6a3
worashf <uppert83@gmail.com>
2022-05-12 23:09:30 +0300

    add contact form validationscript

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ const messageIcon = document.getElementById('message-envelope');
 const portfolioLink = document.getElementById('portfolio-link');
 const aboutLink = document.getElementById('about-link');
 const contactLink = document.getElementById('contact-link');
+const conatctForm = document.getElementById('contact-form');
+const errorMessage = document.getElementById('error-message');
+const email = document.getElementById('email');
+
 function toggleMenu() {
   menu.classList.toggle('open');
   nav.classList.toggle('hide');
@@ -41,6 +45,26 @@ function toggleAboutLink() {
   mainNav.classList.remove('cover-header');
   logo.classList.remove('logo-hide');
 }
+conatctForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const { value } = email;
+  if (value.match(/^[a-z0-9+_.-]+@[a-z0-9.-]+$/)) {
+    conatctForm.submit();
+  } else {
+    errorMessage.textContent = 'Enter email contain only lowercase letter';
+    email.style.border = '1px solid red';
+    errorMessage.style.color = 'red';
+    errorMessage.style.display = 'block';
+    errorMessage.style.marginTop = '10px';
+    errorMessage.style.fontSize = '20px';
+    setTimeout(() => {
+      errorMessage.style.display = 'none';
+      email.style.border = 0;
+    }, 5000);
+  }
+  return false;
+});
+
 aboutLink.addEventListener('click', toggleAboutLink);
 contactLink.addEventListener('click', toggleContactLink);
 
