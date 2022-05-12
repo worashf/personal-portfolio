6b5e876d98deac62c81c509e5c3b75ff4775ecef
worashf <uppert83@gmail.com>
2022-05-12 18:08:13 +0300

    update project description

--- js/modal.js
+++ js/modal.js
@@ -12,7 +12,7 @@ const projects = [
     ],
     liveLink: 'https://worashf.github.io/personal-portfolio/',
     sourceLink: 'https://github.com/worashf/personal-portfolio',
-    description: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    description: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
   }, {
     projectId: 'project-2',
     projectName: 'Profesional Art Printing Data',
